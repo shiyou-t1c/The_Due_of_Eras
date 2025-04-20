7954ce9c79b74b5e34f1f2720dfd687190c89aa8
November20 <wywywy3@outlook.com>
2025-04-20 09:31:08 +0800

    readme and push

--- push.sh
+++ push.sh
@@ -3,3 +3,5 @@ find . -type f -name "*.import" -delete
 git add -A
 git commit
 git push https://gitee.com/t1c/The_Due_of_Eras.git
+git push https://github.com/shiyou-t1c/The_Due_of_Eras
+git push https://t1c@git.code.sf.net/p/the-due-of-eras/code the-due-of-eras-code
