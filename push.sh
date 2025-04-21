be66078bb93e2f59009fe2c6a0f9328ff017d7d0
November20 <wywywy3@outlook.com>
2025-04-21 16:52:02 +0800

    update push.sh

--- push.sh
+++ push.sh
@@ -1,11 +1,12 @@
-rm -rf .import
-find . -type f -name "*.import" -delete
-git add -A
-git commit
-
 git pull git@gitee.com:t1c/The_Due_of_Eras.git
 git pull git@github.com:shiyou-t1c/The_Due_of_Eras.git
 git pull ssh://t1c@git.code.sf.net/p/the-due-of-eras/code
+
+rm -rf .import
+find . -type f -name "*.import" -delete
+
+git add -A
+git commit
 
 git push git@gitee.com:t1c/The_Due_of_Eras.git
 git push git@github.com:shiyou-t1c/The_Due_of_Eras.git
