d37bb70baeffbaefa46a5c40c029098ef2489bde
November20 <wywywy3@outlook.com>
2025-04-27 14:46:03 +0800

    push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-git pull git@gitee.com:t1c/The_Due_of_Eras.git
-git pull git@github.com:shiyou-t1c/The_Due_of_Eras.git
-git pull ssh://t1c@git.code.sf.net/p/the-due-of-eras/code
+git pull --all git@gitee.com:t1c/The_Due_of_Eras.git
+git pull --all git@github.com:shiyou-t1c/The_Due_of_Eras.git
+git pull --all ssh://t1c@git.code.sf.net/p/the-due-of-eras/code
 
 rm -rf .import
 find . -type f -name "*.import" -delete
@@ -8,6 +8,6 @@ find . -type f -name "*.import" -delete
 git add -A
 git commit
 
-git push git@gitee.com:t1c/The_Due_of_Eras.git
-git push git@github.com:shiyou-t1c/The_Due_of_Eras.git
-git push ssh://t1c@git.code.sf.net/p/the-due-of-eras/code
+git push --all git@gitee.com:t1c/The_Due_of_Eras.git
+git push --all git@github.com:shiyou-t1c/The_Due_of_Eras.git
+git push --all ssh://t1c@git.code.sf.net/p/the-due-of-eras/code
